9fd383010e53be4583b3bdc257c37c94cb4d9b66
slydragonn <alogo18m@gmail.com>
2025-07-01 20:35:06 -0500

    exercises 3.15 to 3.18

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import morgan from "morgan";
 import Phone from "./models/phone.js";
 
@@ -32,13 +32,13 @@ app.get("/info", (_, res) => {
   res.send(info);
 });
 
-app.get("/api/persons", (_, res) => {
+app.get("/api/persons", (_, res, next) => {
   Phone.find({}).then((phones) => {
     res.json(phones);
-  });
+  }).catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body.name) {
@@ -56,33 +56,79 @@ app.post("/api/persons", (req, res) => {
 
   phone.save().then((savePhone) => {
     res.json(savePhone);
-  });
+  }).catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
 
   Phone.findById(id)
     .then((phone) => {
-      res.json(phone);
+      if (phone) {
+        res.json(phone);
+      } else {
+        res.status(404).send("<h1>404 Not Found</h1>");
+      }
     })
-    .catch((error) => {
-      console.log(error.message);
-      res.status(404).send("<h1>404 Not Found</h1>");
-    });
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
+  const body = req.body;
 
-  if (person) {
-    persons = persons.filter((person) => person.id !== id);
-    res.status(204).end();
-  } else {
-    res.status(404).end();
+  if (!body.name) {
+    return res.status(400).json({ error: "name is missing" });
   }
+
+  if (!body.number) {
+    return res.status(400).json({ error: "number is missing" });
+  }
+
+  const updatedPhone = {
+    name: String(body.name),
+    number: String(body.number),
+  };
+
+  Phone.findByIdAndUpdate(id, updatedPhone, { new: true })
+    .then((updatedPhone) => {
+      if (updatedPhone) {
+        res.json(updatedPhone);
+      } else {
+        res.status(404).send("<h1>404 Not Found</h1>");
+      }
+    })
+    .catch((error) => next(error));
 });
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  Phone.findByIdAndDelete(id)
+    .then((result) => {
+      if (result) {
+        res.status(204).end();
+      } else {
+        res.status(404).send("<h1>404 Not Found</h1>");
+      }
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send("<h1>400 Bad Request</h1>");
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+// Middleware for handling errors
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}`);
